04b4b909ff80c3ba14f4c0793fa78ba5bf13264c
emacslisp <wudi850620@hotmail.com>
2017-04-15 12:41:36 +1000

    update cpp main

---
+++ cppMain/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+//@example: put all c++ testing code here
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
